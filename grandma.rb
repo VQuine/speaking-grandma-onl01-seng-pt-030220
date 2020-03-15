bd51316578f387dee5d1f5db274c08b9d2881f5c
Kimberly Aviles <01010110.v@gmail.com>
2020-03-15 00:36:34 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,3 +1,10 @@
-def speak_to_grandma(input)
-  return "I LOVE YOU TOO PUMPKIN!" if input == "I LOVE YOU GRANDMA!"
+
+def speak_to_grandma(string_argument)
+  if string_argument == string_argument.upcase
+    return "NO, NOT SINCE 1938!"
+  elsif string_argument != string_argument.upcase
+    return "HUH?! SPEAK UP, SONNY!"
+  elsif string_argument == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+  end
 end
